408ec9bed93342a6339af7cb41c03f41876f3336
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-02-14 12:50:19 +0100

    using sp docker

--- bin/download-remote-data.sh
+++ bin/download-remote-data.sh
@@ -5,10 +5,18 @@ if test -f "$ENV"; then
   echo "$ENV exists."
   [ -d dumps ] || mkdir dumps
   REMOTE_DB=$(grep REMOTE_DB .env | cut -d "=" -f2 | tr -d '"')
-  # npx supabase init
-  pg_dump $REMOTE_DB -n public -Fc > dumps/dump.psql
-  psql -d "postgresql://postgres:postgres@localhost:54322/postgres" -c "drop schema if exists public cascade;;"
-  pg_restore -d postgresql://postgres:postgres@localhost:54322/postgres dumps/dump.psql
+  SP_POSTGRESS_ID=$(docker ps -f name=supabase_db_supabase-local-dev -q)
+  # USING LOCAL POSTGRES
+  # pg_dump $REMOTE_DB -n public -Fc > dumps/dump.psql
+  # psql -d "postgresql://postgres:postgres@localhost:54322/postgres" -c "drop schema if exists public cascade;;"
+  # pg_restore -d postgresql://postgres:postgres@localhost:54322/postgres dumps/dump.psql
+  # USING DOCKER POSTGRESS
+  docker exec $SP_POSTGRESS_ID psql -U postgres -c "drop schema if exists public cascade"
+  docker exec $SP_POSTGRESS_ID pg_dump -U postgres -d $REMOTE_DB -n public -Fc | pg_restore -U postgres -d postgres
+
 else
   echo "$ENV doesn't exist."
 fi
+
+# docker exec -it supabase_db_supabase-local-dev /bin/bash -c "ls"
+# docker cp supabase_db_supabase-local-dev:/dump ./ 
--- bin/upload-local-data.sh
+++ bin/upload-local-data.sh
@@ -5,10 +5,14 @@ if test -f "$ENV"; then
   echo "$ENV exists."
   [ -d dumps ] || mkdir dumps
   REMOTE_DB=$(grep REMOTE_DB .env | cut -d "=" -f2 | tr -d '"')
-  # npx supabase init
-  pg_dump postgresql://postgres:postgres@localhost:54322/postgres -n public -Fc > dumps/dump.psql 
-  psql -d $REMOTE_DB -c "drop schema if exists public cascade;;"
-  pg_restore -d $REMOTE_DB dumps/dump.psql
+  SP_POSTGRESS_ID=$(docker ps -f name=supabase_db_supabase-local-dev -q)
+  # USING LOCAL POSTGRES
+  # pg_dump postgresql://postgres:postgres@localhost:54322/postgres -n public -Fc > dumps/dump.psql 
+  # psql -d $REMOTE_DB -c "drop schema if exists public cascade;;"
+  # pg_restore -d $REMOTE_DB dumps/dump.psql
+  # USING DOCKER OWN PPOSTGRES
+  docker exec $SP_POSTGRESS_ID psql -d $REMOTE_DB -c "drop schema if exists public cascade"
+  docker exec $SP_POSTGRESS_ID pg_dump -U postgres -n public -Fc | pg_restore -U postgres -d $REMOTE_DB
 else
   echo "$ENV doesn't exist."
 fi
